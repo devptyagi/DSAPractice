d73f34988071da7655b210ab4fd83f5ee59bb617
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-08-20 20:53:17 +0530

    Create rotateArrayDTimes.java

---
+++ Arrays/rotateArrayDTimes.java
@@ -0,0 +1,16 @@
+static void reverse(int[] a, int i, int j) {
+    while(i < j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        i++;
+        j--;
+    }
+} 
+
+static void rotateArr(int arr[], int d, int n)
+{
+    reverse(arr, 0, d-1);
+    reverse(arr, d, n-1);
+    reverse(arr, 0, n-1);
+}
