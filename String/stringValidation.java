679cf4206525c261475a1d7d515f13628d2c9e39
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-11-08 19:02:19 +0530

    Update stringValidation.java

--- String/stringValidation.java
+++ String/stringValidation.java
@@ -1,10 +1,14 @@
-/*Given a string s representing a password, you need to check if the string is valid or not. A valid string has the following properties:
+/*
+
+Given a string s representing a password, you need to check if the string is valid or not. A valid string has the following properties:
 
 String must have the length greater than or equal to 10.
 String must contain at least 1 numeric character.
 String must contain at least 1 uppercase character.
 String must contain at least 1 lowercase character.
-String must contain at least 1 special character from @#$-*.*/
+String must contain at least 1 special character from @#$-*.
+
+*/
 
 public static boolean validate(String str) {
     if(str.length() < 10) return false;
